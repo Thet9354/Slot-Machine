2b58c8e054e437fdc8d442937233fb2117d28bf8
PhoonThetPine <thetpine254@gmail.com>
2023-10-28 17:50:24 +0800

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Phoon Thet Pine on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
